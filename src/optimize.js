8a366a944116b2d3e35f8bc4974604e433f2e41f
Ignacio Gutierrez <pepo.dev@gmail.com>
2018-11-05 12:50:49 -0300

    load food and run

--- src/optimize.js
+++ src/optimize.js
@@ -6,29 +6,36 @@ const constraints = require('./resources/constraints')
 
 const baseItem = nutrients.map(nutrient => ({[nutrient]: 0})).reduce((a,  b) => ({...a, ...b}))
 
-const item = profile => ({
+const item = (name, profile) => ({
+  [name]: 1.0,
   'amount': 100,
   ...baseItem,
   ...profile
 })
 
-fs.readdir(`${__dirname}/resources/food`, (err, files) => {
-  if (err) {
-    throw new Error(err)
-  }
-  files.forEach(file => {
-    console.log(file);
-  });
+const getFood = () => new Promise(function(resolve, reject) {
+  fs.readdir(`${__dirname}/resources/food`, (err, files) => {
+    if (err) {
+      return reject(err)
+    }
+    let food = {}
+    files.forEach(fileName => {
+      const foodName = fileName.split('.')[0]
+      food[foodName] = item(foodName, require(`${__dirname}/resources/food/${fileName}`))
+    });
+    resolve(food)
+  })
 })
 
-// const model = {
-//     "optimize": "calories",
-//     "opType": "min",
-//     constraints,
-//     variables
-// }
+getFood()
+.then(food => {
+  const model = {
+      "optimize": "calories",
+      "opType": "min",
+      constraints,
+      variables: food
+  }
 
-
-// results = solver.Solve(model)
-//
-// console.log(results)
+  const results = solver.Solve(model)
+  console.log(results)
+})
